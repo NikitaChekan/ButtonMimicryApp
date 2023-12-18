b48ef7c84dc3fdf6e795c2f5db2075bfaa63bbe4
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-18 16:22:11 +0300

    Initial Commit

---
+++ ButtonMimicryApp/ButtonMimicryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonMimicryAppApp.swift
+//  ButtonMimicryApp
+//
+//  Created by jopootrivatel on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonMimicryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
