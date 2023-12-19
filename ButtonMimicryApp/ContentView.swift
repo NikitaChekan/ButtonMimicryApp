467a5388e1524d5532e0a8d6ec9319e5592c8b76
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-12-19 17:30:33 +0300

    mission complete

--- ButtonMimicryApp/ContentView.swift
+++ ButtonMimicryApp/ContentView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Namespace private var animation
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if isExpanded {
+            ZStack(alignment: .topLeading) {
+                Color.blue
+                    .cornerRadius(25)
+                    .matchedGeometryEffect(id: "button", in: animation)
+                    .frame(
+                        width: isExpanded ? 350 : 100,
+                        height: isExpanded ? 450 : 50
+                    )
+                Button(action: {
+                    withAnimation(.spring) {
+                        isExpanded.toggle()
+                    }
+                }) {
+                    HStack {
+                        Image(systemName: "arrowshape.backward.fill")
+                        Text("Back")
+                            .font(.title3.bold())
+                    }
+                    .padding()
+                    .foregroundStyle(.white)
+                    .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
+                }
+            }
+        } else {
+            Button(action: {
+                withAnimation(.spring) {
+                    isExpanded.toggle()
+                }
+            }) {
+                Color.blue
+                    .cornerRadius(15)
+                    .matchedGeometryEffect(id: "button", in: animation)
+                    .frame(
+                        width: !isExpanded ? 100 : 350,
+                        height: !isExpanded ? 50 : 450
+                    )
+                    .overlay(
+                        Text("Open")
+                            .font(.title3.bold())
+                            .foregroundStyle(.white)
+                            .matchedGeometryEffect(id: "Text", in: animation, properties: .position, anchor: .leading)
+                    )
+                    .padding()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: isExpanded ? .center : .bottomTrailing)
         }
-        .padding()
     }
 }
 
